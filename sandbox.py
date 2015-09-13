1bb689a9ac8f69c64b83ee443545918e1a82ff0c
Fernando Maymi <Fernando@Fernandos-MBP.home>
2015-09-13 11:00:24 -0400

    Added command line args for sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,15 @@
 import sys
 
-code = open('code.in','rb').read()
-data = open('data.in', 'rb').read()
+if len(sys.argv)>2:
+    code = open(sys.argv[1],'rb').read()
+    data = open(sys.argv[2], 'rb').read()
+elif len(sys.argv)=2:
+    code = open(sys.argv[1],'rb').read()
+    data = open('data.in', 'rb').read()
+elif len(sys.argv)<2:
+    code = open('code.in','rb').read()
+    data = open('data.in', 'rb').read()
+
 
 namespace = {}
 namespace['__builtins__'] = {'True':True, 'False':False, 'None':None, 'data':data}
